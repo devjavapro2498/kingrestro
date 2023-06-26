82c96b57c448a34e3d9a46f701ea7c5f8ddba629
devjavapro2498 <cultivatejava@gmail.com>
2023-06-27 01:29:36 +0530

    Initial commit

---
+++ src/main/java/com/kr/kingrestro/KingrestroApplication.java
@@ -0,0 +1,13 @@
+package com.kr.kingrestro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingrestroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingrestroApplication.class, args);
+    }
+
+}
