3578a3eca2363154a87f2f6fdb1f8e3df74eac12
devjavapro2498 <cultivatejava@gmail.com>
2023-07-01 17:22:44 +0530

    forth commit

--- src/main/java/com/kr/kingrestro/controller/RestController.java
+++ src/main/java/com/kr/kingrestro/controller/RestController.java
@@ -29,4 +29,69 @@ public class RestController {
             return "error";
         }
     }
+    @GetMapping("/service")
+    public String ServiceAvail(Model model){
+        try{
+            return "service";
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "An error occurred while processing the template.");
+            return "error";
+        }
+
+    }
+
+    @GetMapping("/menu")
+    public String MenuDetails(Model model){
+        try{
+            return "menu";
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "An error occurred while processing the template.");
+            return "error";
+        }
+
+    }
+
+
+    @GetMapping("/booking")
+    public String Bookings(Model model){
+        try{
+            return "booking";
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "An error occurred while processing the template.");
+            return "error";
+
+        }
+    }
+
+    @GetMapping("/team")
+    public String Team(Model model){
+        try{
+            return "team";
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "An error occurred while processing the template.");
+            return "error";
+
+        }
+    }
+    @GetMapping("/testimonial")
+    public String Testimonials(Model model){
+        try{
+            return "testimonial";
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "An error occurred while processing the template.");
+            return "error";
+
+        }
+    }
+    @GetMapping("/contact")
+    public String ContactDetails(Model model){
+        try{
+            return "contact";
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "An error occurred while processing the template.");
+            return "error";
+
+        }
+    }
+
 }
