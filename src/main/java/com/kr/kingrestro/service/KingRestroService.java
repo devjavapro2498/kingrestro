a6e04eedba35474e3158a4599a01474eac37d382
devjavapro2498 <cultivatejava@gmail.com>
2023-07-11 15:28:19 +0530

    Email service code commit

--- src/main/java/com/kr/kingrestro/service/KingRestroService.java
+++ src/main/java/com/kr/kingrestro/service/KingRestroService.java
@@ -5,6 +5,11 @@ import com.kr.kingrestro.repository.KingRestroRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
 @Service
 public class KingRestroService {
     @Autowired
@@ -12,5 +17,52 @@ public class KingRestroService {
 
     public void addEmp(BookingEntity bookingEntity) {
         kingRestroRepo.save(bookingEntity);
+        System.out.println("Prepare to Send Message");
+        String message="HI"+"/"+ bookingEntity.getName()+
+                "Thank you for Booking .Your Booking is Confirmed for 7 Star King Restaurant . Our CEO Mr. Mohit  welcome you because   you're very Special guest so we will provide you great service." +
+                "Thank you" +
+                "BooKing Time:" +bookingEntity.getDatetime() + "Customer Name: " +bookingEntity.getName()+ "No of People: " +bookingEntity.getNoOfPeople();
+        String subject ="KingRestro: Confirmation";
+        String to =bookingEntity.getEmail();
+        String from = "cultivatejava@gmail.com";
+        sendEmail(message,subject,to,from);
+    }
+
+    private static void sendEmail(String message, String subject, String to, String from) {
+        //varianle for gmail host
+        String host ="smtp.gmail.com";
+        Properties properties= System.getProperties();
+        System.out.println("PROPERTIES" +properties);
+        //Setting Important info to properties object
+        //host set
+        properties.put("mail.smtp.host",host);
+        properties.put("mail.smtp.port","465");
+        properties.put("mail.smtp.ssl.enable","true");
+        properties.put("mail.smtp.auth","true");
+        //Step :1 To get the Session Object
+        Session session= Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("cultivatejava@gmail.com" ,"***********");
+            }
+        });
+//Compose the message
+        session.setDebug(true);
+        MimeMessage mimeMessage = new MimeMessage(session);
+        try {
+            //from email
+            mimeMessage.setFrom(from);
+            mimeMessage.addRecipients(Message.RecipientType.TO, String.valueOf(new InternetAddress(to)));
+            mimeMessage.setSubject(subject);
+            mimeMessage.setText(message);
+            // Step:3  Send
+            Transport.send(mimeMessage);
+            System.out.println("Successfully Send");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
+
